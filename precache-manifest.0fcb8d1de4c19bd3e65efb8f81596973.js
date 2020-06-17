f4b8a1fda8549d026c2d998960d2ffe0d816d91e
Festus Ebin <ebinfestus@gmail.com>
2020-06-17 12:33:49 +0100

    Updates

---
+++ precache-manifest.0fcb8d1de4c19bd3e65efb8f81596973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bcb3e2ffc1eff8407b732e9a4d4e31",
+    "url": "/festusebin/medicare-dashboard-react/index.html"
+  },
+  {
+    "revision": "37481bcea45bd066756e",
+    "url": "/festusebin/medicare-dashboard-react/static/css/main.26e255f6.chunk.css"
+  },
+  {
+    "revision": "8aec2fd8bf77bfe9641e",
+    "url": "/festusebin/medicare-dashboard-react/static/js/2.0e74b340.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/festusebin/medicare-dashboard-react/static/js/2.0e74b340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37481bcea45bd066756e",
+    "url": "/festusebin/medicare-dashboard-react/static/js/main.3b11d467.chunk.js"
+  },
+  {
+    "revision": "4fcdc7aabb1b0fb8e442",
+    "url": "/festusebin/medicare-dashboard-react/static/js/runtime-main.09ab07a8.js"
+  },
+  {
+    "revision": "59a017e02c023d190a2099b7f346b551",
+    "url": "/festusebin/medicare-dashboard-react/static/media/univelcity-logo-green.59a017e0.svg"
+  }
+]);
