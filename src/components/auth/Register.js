85044f3cef840e645d1a3df87657ea0a2aace4e7
Joseph Ebin <josefophe@gmail.com>
2020-06-17 11:53:41 +0100

    initial commit

---
+++ src/components/auth/Register.js
@@ -0,0 +1,155 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { registerUser } from "../../actions/authActions";
+
+import "./Auth.scss";
+import Univelcity from '../common/Univelcity';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    // If logged in and user navigates to Register page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+  };
+
+  render() {
+    const { errors } = this.state;
+
+    return (
+      <div className="base-wrapper">
+      <Univelcity />
+        <div className="auth-header">Register your account</div>
+        <form className="auth-form" noValidate onSubmit={this.onSubmit}>
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Full Name</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.name}
+                error={errors.name}
+                id="name"
+                type="text"
+                className="auth-input"
+              />
+              <div className="auth-error">{errors.name}</div>
+            </label>
+          </div>
+
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Email address</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.email}
+                error={errors.email}
+                id="email"
+                type="email"
+                className="auth-input"
+              />
+              <div className="auth-error">{errors.email}</div>
+            </label>
+          </div>
+
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Password</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.password}
+                error={errors.password}
+                id="password"
+                type="password"
+                className="auth-input"
+              />
+              <div className="auth-error">{errors.password}</div>
+            </label>
+          </div>
+
+          <div className="auth-group">
+            <label>
+              <div className="auth-label">Confirm Password</div>
+              <input
+                onChange={this.onChange}
+                value={this.state.password2}
+                error={errors.password2}
+                id="password2"
+                type="password"
+                className="auth-input"
+              />
+              <div className="auth-error">{errors.password2}</div>
+            </label>
+          </div>
+
+
+          <div>
+            <button type="submit" className="auth-button">
+              Sign up
+            </button>
+          </div>
+          <div className="bottom-group">
+            <p>Already have an account,
+              <Link to="/" className="link">
+                Login
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Register));
---
+++ src/components/common/Univelcity.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../../img/univelcity-logo-green.svg";
+import "./Univelcity.scss";
+
+const Univelcity = () => {
+  return (
+    <div className="logo">
+      <img className="center" src={logo} alt="Logo" />
+    </div>
+  );
+};
+
+export default Univelcity;
